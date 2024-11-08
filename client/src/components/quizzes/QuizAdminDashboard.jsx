260213339580774dc0d3013fdcc43972e923e65c
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-11-08 19:19:37 +0530

    "Update text color of Quiz Admin Dashboard header to white"

--- client/src/components/quizzes/QuizAdminDashboard.jsx
+++ client/src/components/quizzes/QuizAdminDashboard.jsx
@@ -150,7 +150,7 @@ const QuizAdminDashboard = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4 pt-6 pb-12">
-      <h1 className="text-3xl font-bold text-gray-900 mb-4">
+      <h1 className="text-3xl font-bold text-white mb-4">
         Quiz Admin Dashboard
       </h1>
 
