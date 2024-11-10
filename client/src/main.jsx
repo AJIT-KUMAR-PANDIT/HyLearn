644a659a48259e9eb9bb83a51fa212e102c08772
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-11-10 16:42:46 +0530

    "Updated server and frontend URL exports to use VITE_SERVER and VITE_FRONTEND environment variables"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,11 @@ import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 import "./index.css";
 
-export const server = "http://localhost:5000";
+export const server = `${import.meta.env.VITE_SERVER}`;
 // "https://hylearn.onrender.com";
 
-export const frontend = "https://hylearn.vercel.app";
+export const frontend = `${import.meta.env.VITE_FRONTEND}`;
+// "https://hylearn.vercel.app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
